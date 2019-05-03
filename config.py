9ac628157603565745dc674b07159d5830bbdbf6
Brian Carroll <briancarroll112@gmail.com>
2019-05-03 17:20:33 -0400

    db set stup sijf

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
+from secrets import *
 
 class Config(object):
-    PG_USERNAME = os.environ.get('PG_USERNAME') or 'PostgreSQL'
-    PG_PASSWORD = os.environ.get('PG_PASSWORD') or 'PostgreSQL'
+    PG_USERNAME = os.environ.get('PG_USERNAME') or PG_USERNAME_SECRETS
+    PG_PASSWORD = os.environ.get('PG_PASSWORD') or PG_PASSWORD_SECRETS
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'like-really-secret-dude'
