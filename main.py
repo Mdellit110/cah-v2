6ffe37b5f64a2c3891988006f9909fe771e0e8c2
matthew dellitalia <matthewdellitalia@ool-ad020649.dyn.optonline.net>
2019-05-03 12:16:57 -0400

    add requirements and set up boilerplate

---
+++ main.py
@@ -0,0 +1,23 @@
+import graphene
+from graphene_sqlalchemy import SQLAlchemyObjectType
+from fastapi import FastAPI
+from starlette.graphql import GraphQLApp
+from models import *
+
+class User(SQLAlchemyObjectType):
+    class Meta:
+        model = UserModel
+        # only return specified fields
+        only_fields = ("name",)
+        # exclude specified fields
+        exclude_fields = ("last_name",)
+
+class Query(graphene.ObjectType):
+    users = graphene.List(User)
+
+    def resolve_users(self, info):
+        query = User.get_query(info)  # SQLAlchemy query
+        return query.all()
+
+app = FastAPI()
+app.add_route("/", GraphQLApp(schema=graphene.Schema(query=Query)))
