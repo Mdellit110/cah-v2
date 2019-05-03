dd474f902956ae618f8dfb36c610d1e505a51e5c
Robert Morrissey <rbrtmorrissey86@gmail.com>
2019-05-03 13:33:25 -0400

    models for cards

--- models.py
+++ models.py
@@ -9,3 +9,18 @@ class UserModel(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     last_name = Column(String)
+
+class WhiteCardModel(Base):
+    __tablename__ = 'white_cards'
+    id = Column(Integer, primary_key=True)
+    deck = Column(String)
+    icon = Column(String)
+    text = Column(String)
+
+class BlackCardModel(Base):
+    __tablename__ = 'black_cards'
+    id = Column(Integer, primary_key=True)
+    pick = Column(Integer)
+    deck = Column(String)
+    text = Column(String)
+    icon = Column(String)
